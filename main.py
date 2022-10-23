cc5490b4ef0d13f566fd6264224dd21992a87d05
Sveta <margaritka2132@mail.ru>
2022-10-23 19:16:03 +0400

    added changes

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ from bot_commands import *
 
 app = ApplicationBuilder().token("5742593421:AAGOqKo_h8YtCP4tvx3R-56HSdMMb5mhQ24").build()
 
-  
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("play", play))
-
-
 
 app.run_polling()
 
